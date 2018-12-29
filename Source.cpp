6bbba5e140cf0cbb416ec40730c22a5bb5c22ca5
Pluser761 <danchik761@gmail.com>
2018-12-29 14:33:54 +0300

    get copy

---
+++ Source.cpp
@@ -0,0 +1,17 @@
+#include "matrix.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	vector<vector<int>> st(3);
+	for (int i = 0; i < 3; i++)
+		for (int j = 0; j < 3; j++)
+			st[i].push_back(i * 3 + j);
+	matrix<4, int> a(st);
+	cout << a << "\n" << "determine is " << a.deter(0);
+	system("pause");
+	return 0;
+}
